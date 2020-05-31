cf3f131c00a156fbc00c0e291dafb8a260a14073
Matt B <matt.bodle@qantasloyalty.com>
2020-06-01 09:20:14 +1000

    Added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: "javascript",
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    
+    arrowParens: "always",
+    tabWidth: 3,
+    useTabs: false
+};
